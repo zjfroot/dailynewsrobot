c29661abaddc94d6b82e023c12d7dff1ee2c5c57
Jifeng Zhang <zjfroot@gmail.com>
2011-01-07 13:25:39 +0100

    A few refactor of the xml fetcher

--- modules/xmlfetcher.py
+++ modules/xmlfetcher.py
@@ -13,24 +13,33 @@ class XmlFetcher:
         self.section = section
         
     def get(self):
-        #First try to load the xml from cache
+        #generate the key that represent which news source to fetch.
         key = self.source + '_' +self.section
+        
+        disableCache = True
+        if disableCache:
+            return self.__download(key)
+    
+        #First try to load the xml from cache
+        
         xml = memcache.get(key)
         
         #If found, return the xml, otherwise download from news provider's rss url
         if xml is not None:
             return xml
         else:
-            xml = self.__download()
+            xml = self.__download(key)
             memcache.add(key,xml,600)
             return xml
 
     #download from rss    
-    def __download(self):
-        if self.source == "dn":
-            if self.section == "top":
-                url = "http://www.dn.se/m/rss/senaste-nytt"
+    def __download(self,key):
+        map = { 'dn_top': "http://www.dn.se/m/rss/senaste-nytt",
+                'dn_stockholm': "http://www.dn.se/sthlm/m/rss/senaste-nytt",
         
+                }
+        
+        url = map[key]
         try:
             response = urllib2.urlopen(url)
             result = response.read()
